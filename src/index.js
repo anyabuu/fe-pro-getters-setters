b3b0789206ad2f443897ce4191674fe6199c4354
Anya Bu <annabukhtiiarova@gmail.com>
2022-07-13 16:41:00 +0300

    getter made

--- src/index.js
+++ src/index.js
@@ -21,3 +21,74 @@ export const school = {
     7: new Student('Ivan', [76, 89, 78, 98, 98, 99, 89, 96]),
   },
 };
+
+const studentArr = Object.values(school.students);
+const getAverage = (numbers) => {
+  const sum = numbers.reduce((acc, number) => acc + number, 0);
+  const length = numbers.length;
+  return Math.round(sum / length);
+};
+
+Object.defineProperty(school, 'aGradeStudents', {
+  get() {
+    let gradeFilter = studentArr.filter(function (item) {
+      if (getAverage(item.grades) <= 100 && getAverage(item.grades) >= 90) {
+        return item;
+      }
+    });
+
+    let gradeNames = gradeFilter.map(function (item) {
+      return item.name;
+    });
+
+    return gradeNames.join(', ');
+  },
+});
+
+Object.defineProperty(school, 'bGradeStudents', {
+  get() {
+    let gradeFilter = studentArr.filter(function (item) {
+      if (getAverage(item.grades) <= 89 && getAverage(item.grades) >= 75) {
+        return item;
+      }
+    });
+
+    let gradeNames = gradeFilter.map(function (item) {
+      return item.name;
+    });
+
+    return gradeNames.join(', ');
+  },
+});
+
+Object.defineProperty(school, 'cGradeStudents', {
+  get() {
+    let gradeFilter = studentArr.filter(function (item) {
+      if (getAverage(item.grades) <= 75 && getAverage(item.grades) >= 60) {
+        return item;
+      }
+    });
+
+    let gradeNames = gradeFilter.map(function (item) {
+      return item.name;
+    });
+
+    return gradeNames.join(', ');
+  },
+});
+
+Object.defineProperty(school, 'dGradeStudents', {
+  get() {
+    let gradeFilter = studentArr.filter(function (item) {
+      if (getAverage(item.grades) <= 59 && getAverage(item.grades) >= 0) {
+        return item;
+      }
+    });
+
+    let gradeNames = gradeFilter.map(function (item) {
+      return item.name;
+    });
+
+    return gradeNames.join(', ');
+  },
+});
